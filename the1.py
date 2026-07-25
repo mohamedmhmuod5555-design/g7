3d1ea4302ef7043b87a76b2dd76a3fd21258b93c
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-25 04:23:51 -0700

    Update the1.py

--- the1.py
+++ the1.py
@@ -1,14 +1,14 @@
 import random
 import streamlit as st 
-if 'num' not in session_state:
-  session_state.num
+if 'num' not in st.session_state:
+  st.session_state.num
 if 'num1' not in session_state:
- session_state.num1=random.randint(1,20)
- session_state.num2=random.randint(1,20)
- session_state.sign=random.choice(['+','-','*','/'])
-num1=session_state.num1
-num2=session_state.num2
-sign=session_state.sign
+ st.session_state.num1=random.randint(1,20)
+ st.session_state.num2=random.randint(1,20)
+ st.session_state.sign=random.choice(['+','-','*','/'])
+num1=st.session_state.num1
+num2=st.session_state.num2
+sign=st.session_state.sign
 if sign=='+':
  sc=num1+num2
 if sign=='-':
@@ -23,13 +23,13 @@ number=st.number_input("ادخل النتيجه ")
 if st.button("تاكيد الاجابه "):
  if number==sc:
   print("you are winner ")
-  session_state.num=+1
+  st.session_state.num=+1
  else:
   print("your answer is not true ") 
-  del  session_state.num
+  del  st.session_state.num
 if st.button(" السؤال التالي "):
- del session_state.num1 
- del session_state.num2
- del session_state.sign
+ del st.session_state.num1 
+ del st.session_state.num2
+ del st.session_state.sign
  st.rerun()
 st.write(num)
