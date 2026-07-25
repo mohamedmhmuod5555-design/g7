65680adfe00855c3fa4510f97603cf78fba51fa1
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-25 05:13:38 -0700

    Update the1.py

--- the1.py
+++ the1.py
@@ -26,7 +26,7 @@ st.write(num1,sign,num2)
 number = st.number_input("أدخل النتيجة", key=f"input_{st.session_state.num1}_{st.session_state.num2}")
 if st.button("تاكيد الاجابه "):
   st.session_state.num += 1
- if number==sc:
+  if number==sc:
    st.success("اجابتك صحيحه احسنت  ")
    st.session_state.num+=1
  else:
